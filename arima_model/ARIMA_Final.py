b901e371a0c72195437678c2476f1d0e08bcaee0
John O'Connell <oconnell.j@northeastern.edu>
2023-08-22 20:15:04 -0400

    added rolling forecast technique to ARIMA model

--- arima_model/ARIMA_Final.py
+++ arima_model/ARIMA_Final.py
@@ -1,8 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[136]:
-
+# %%
 import warnings
 warnings.filterwarnings('ignore')
 #Data Manipulation and Treatment
@@ -21,20 +17,14 @@ from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf, plot_predict
 from pmdarima import auto_arima
 
-
-# In[137]:
-
-
+# %%
 #Download stock data
 df = yf.download('aapl', start='2010-01-01', end='2020-01-01')
 df = df.resample('D').asfreq()
 df = df.ffill()
 df.head()
 
-
-# In[138]:
-
-
+# %%
 #Plot closing price of stock data
 plt.figure(figsize=(10,6))
 plt.grid(True)
@@ -43,10 +33,7 @@ plt.xlabel('Date', fontsize=12)
 plt.ylabel('Close', fontsize=12)
 plt.title('Apple Closing Price')
 
-
-# In[139]:
-
-
+# %%
 # Checking Trend and Seasonality
 plt.figure(figsize=(15,7))
 plt.grid(True)
@@ -58,10 +45,7 @@ plt.ylabel('Close', fontsize=12)
 plt.legend(loc='best')
 plt.title('Rolling Mean & Standard Deviation')
 
-
-# In[140]:
-
-
+# %%
 # Checking Trend and Seasonality
 from statsmodels.tsa.seasonal import seasonal_decompose 
 decomposition = seasonal_decompose(df['Close'], model='multiplicative', period=30)
@@ -72,10 +56,7 @@ residual = decomposition.resid
 fig = decomposition.plot()
 fig.set_size_inches(14, 7)
 
-
-# In[141]:
-
-
+# %%
 #Running ADF Test on raw data
 def ADF_test(timeseries):
     print("Results of Dickey-Fuller Test:")
@@ -98,10 +79,7 @@ def ADF_test(timeseries):
         print('Reject the Null Hypothesis: Data is stationary.')
 ADF_test(df['Close'])
 
-
-# In[142]:
-
-
+# %%
 #Split data into train and training set
 df_train, df_test = df[:int(len(df)*0.8)], df[int(len(df)*0.8):]
 
@@ -116,10 +94,7 @@ plt.ylabel('Close')
 plt.legend()
 plt.title('Apple Closing Price')
 
-
-# In[143]:
-
-
+# %%
 #Apply first order differencing to data
 df_train_diff = df_train['Close'].diff().dropna()
 
@@ -131,32 +106,20 @@ plt.xlabel('Date', fontsize=12)
 plt.ylabel('Close', fontsize=12)
 plt.title('First Order Differencing')
 
-
-# In[144]:
-
-
+# %%
 #Perform ADF test on differenced data
 ADF_test(df_train_diff)
 
-
-# In[145]:
-
-
+# %%
 #Plot PACF
 plt.rc("figure", figsize=(10,6))
 pacf = plot_pacf(df_train_diff)
 
-
-# In[146]:
-
-
+# %%
 #Plot ACF
 acf = plot_acf(df_train_diff)
 
-
-# In[147]:
-
-
+# %%
 #Fit ARIMA model to data
 model_arima= auto_arima(df_train['Close'],trace=True, 
                         error_action='ignore', 
@@ -170,9 +133,7 @@ model_arima= auto_arima(df_train['Close'],trace=True,
 model_arima.fit(df_train['Close'])
 
 
-# In[148]:
-
-
+# %%
 #Use model to make predictions on test data
 prediction_arima=model_arima.predict(len(df_test))
 y_pred["ARIMA Model Prediction"]=prediction_arima.dropna()
@@ -188,10 +149,7 @@ plt.ylabel('Close', fontsize=12)
 plt.legend(loc='best')
 plt.title('ARIMA(2,1,2) Model Results')
 
-
-# In[149]:
-
-
+# %%
 #Plot Results
 plt.figure(figsize=(15,7))
 plt.plot(df_test["Close"], color='red', label='Test Data')
@@ -202,19 +160,68 @@ plt.ylabel('Close', fontsize=12)
 plt.legend(loc='best')
 plt.title('ARIMA(2,1,2) Model Results')
 
-
-# In[150]:
-
-
+# %%
 #Calculate Mean Squared Error and Mean Absolute Percentage Error
 mse_arima= mean_squared_error(y_pred["Close"],y_pred["ARIMA Model Prediction"])
 mape_arima=mean_absolute_percentage_error(y_pred["Close"],y_pred["ARIMA Model Prediction"])
 print("Mean Square Error ARIMA: ",mse_arima)
 print("Mean Absoulute Percentage Error ARIMA: ",mape_arima)
 
+# %%
+df_train_new = df_train["Close"]
 
-# In[ ]:
+# %%
+history = [x for x in df_train["Close"]]
+test_data = [x for x in df_test["Close"]]
+model_predictions = []
+N_test_observations = len(df_test)
 
+# %%
+from statsmodels.tsa.arima.model import ARIMA
+import warnings
+from statsmodels.tools.sm_exceptions import ConvergenceWarning
 
+warnings.simplefilter('ignore', ConvergenceWarning)
+
+for time_point in range(N_test_observations):
+    model = ARIMA(history, order=(2,1,2))
+    model_fit = model.fit()
+    output = model_fit.forecast()
+    yhat = output[0]
+    model_predictions.append(yhat)
+    true_test_value = test_data[time_point]
+    history.append(true_test_value)
+
+# %%
+# update ARIMA Model Predictions
+y_pred["ARIMA Model Prediction"]=model_predictions
+
+#Plot results
+plt.figure(figsize=(15,7))
+plt.plot(df_train["Close"], label='Train Data')
+plt.plot(df_test["Close"], color='red', label='Test Data')
+plt.plot(y_pred["ARIMA Model Prediction"], color='green', label='Prediction')
+plt.grid(True)
+plt.xlabel('Date', fontsize=12)
+plt.ylabel('Close', fontsize=12)
+plt.legend(loc='best')
+plt.title('ARIMA(2,1,2) Model Results')
+
+# %%
+mse_arima= mean_squared_error(y_pred["Close"],y_pred["ARIMA Model Prediction"])
+mape_arima=mean_absolute_percentage_error(y_pred["Close"],y_pred["ARIMA Model Prediction"])
+print("Mean Square Error ARIMA: ",mse_arima)
+print("Mean Absoulute Percentage Error ARIMA: ",mape_arima)
+
+# %%
+#Plot Results
+plt.figure(figsize=(15,7))
+plt.plot(df_test["Close"], color='red', label='Test Data')
+plt.plot(y_pred["ARIMA Model Prediction"], color='green', label='Prediction')
+plt.grid(True)
+plt.xlabel('Date', fontsize=12)
+plt.ylabel('Close', fontsize=12)
+plt.legend(loc='best')
+plt.title('ARIMA(2,1,2) Model Results')
 
 
